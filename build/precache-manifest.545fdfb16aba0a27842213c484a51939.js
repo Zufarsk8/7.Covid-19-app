3a39cf147eb944f3f57676d10c14b32bbdbe07bf
Zufar <zufar.adnan@gmail.com>
2020-05-15 01:06:40 +1000

    first commit

---
+++ build/precache-manifest.545fdfb16aba0a27842213c484a51939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7ca85ed05940ceba4a91c79839d8a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e922b620510d98393c",
+    "url": "/static/js/2.351b8c2f.chunk.js"
+  },
+  {
+    "revision": "4464eb154f2bab0d31e275750643f12c",
+    "url": "/static/js/2.351b8c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4948efa546a23ebcb4c9",
+    "url": "/static/js/main.75e53e6a.chunk.js"
+  },
+  {
+    "revision": "87743b05aa0f85073652",
+    "url": "/static/js/runtime-main.ccc94d52.js"
+  }
+]);
